701e46c0c3048a0e8093655f151ca95a79045e78
IztihadRafid <iztihadrafid10@gmail.com>
2024-12-30 19:49:13 +0600

    menucollection, review collection added, finding data from bakend and showing to client side

---
+++ .history/index_20241230135700.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const cors = require("cors")
+const app = express()
+require("dotenv").config();
+const port = process.env.PORT || 5000
+
+
+//middleware
+app.use(cors())
+app.use(express.json())
+
+
+
+app.get('/', (req, res) => {
+  res.send('Bistro Boss is ACTIVE')
+})
+
+app.listen(port, () => {
+  console.log(`Bistro Boss is running on ${port}`)
+})
