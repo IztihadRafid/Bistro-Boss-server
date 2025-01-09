eaa100b83f7f2aa8f3734f36433f9ffd0babd590
IztihadRafid <iztihadrafid10@gmail.com>
2025-01-09 21:49:12 +0600

    payment method by stripe added and made a payment related api

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const cors = require("cors")
 const app = express()
 require("dotenv").config();
+const stripe= require('stripe')(process.env.STRIPE_SECRET_KEY)
 const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 5000
 
@@ -36,6 +37,7 @@ async function run() {
         const reviewCollection = client.db("bistroDb").collection("reviews");
         const cartCollection = client.db("bistroDb").collection("carts")
         const userCollection = client.db("bistroDb").collection('users');
+        const paymentCollection = client.db("bistroDb").collection('payments');
 
         //JWT related Api
         app.post('/jwt', async (req, res) => {
@@ -215,7 +217,46 @@ async function run() {
         })
 
 
+        //Payment Histroy
+        app.get('/payments/:email',verifyToken,async(req,res)=>{
+            const query = {email: req.params.email}
+            if(req.params.email !==req.decoded.email){
+                return res.status(403).send({message: 'forbidden access'});
+            }
+            const result = await paymentCollection.find(query).toArray();
+            res.send(result)
+        })
 
+
+
+        //Payment INtent
+        app.post("/create-payment-intent",async(req,res)=>{
+            const {price} = req.body;
+            const amount = parseInt(price*100)
+            const paymentIntent = await stripe.paymentIntents.create({
+                amount:amount,
+                currency: "usd",
+                payment_method_types: ['card']
+            });
+            res.send({
+                clientSecret: paymentIntent.client_secret
+            })
+        })
+        //Payment realted API
+        app.post('/payments',async(req,res)=>{
+            const payment = req.body;
+            const paymentResult = await paymentCollection.insertOne(payment)
+
+            //delete each item from the cart
+            console.log("payment info: ",payment);
+            const query = {_id:{
+                $in: payment.cartIds.map(id=> new ObjectId(id))
+            }}
+            const deleteResult = await cartCollection.deleteMany(query);
+
+            res.send({paymentResult,deleteResult}) 
+        })
+        
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
